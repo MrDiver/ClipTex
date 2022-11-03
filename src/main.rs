da44ade9c6d8944e03a33c03955dd7210b46def4
MrDiver <mrdiverlp@gmail.com>
2022-11-03 20:24:32 +0100

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use arboard::{Clipboard, ImageData};
 use cairo::ImageSurface;
 use clipboard_master::{CallbackResult, ClipboardHandler};
 use env_logger;
-use inputbot::{KeySequence, KeybdKey::*, MouseButton::*};
-use log::{debug, error, info, log_enabled, warn};
+use log::{debug, error};
 use notify_rust::Notification;
-use std::{borrow::Cow, fs::File};
-use std::{fs, process::Command};
+use std::{borrow::Cow, fs};
 use tectonic;
 
 fn has_latex_macro(input: &str) -> bool {
@@ -24,31 +22,67 @@ fn string_in_template(input: &str) -> String {
     doc
 }
 
-fn tectonic_rendering(latex_code: &str) -> (Vec<u8>, i32, i32) {
+fn tectonic_rendering(latex_code: &str) -> Option<(Vec<u8>, i32, i32)> {
     /* Render latex to PDF */
     let scale = 5.0;
-    let mut pdf_vec = tectonic::latex_to_pdf(latex_code).expect("Couldn't compile Latex code");
+    let pdf_result = tectonic::latex_to_pdf(latex_code);
+    if let Err(error) = pdf_result {
+        error.dump_uncolorized();
+        error!("Failed: {}", error.description());
+        return None;
+    }
+    let mut pdf_vec = pdf_result.unwrap();
     let bytes = pdf_vec.as_mut_slice();
 
     // let mut file = File::create("compare.pdf").expect("Couldn't create file");
     // file.write_all(bytes).expect("Couldn't write to pdf file");
 
     /* Parse PDF data with Poppler */
-    let document = poppler::PopplerDocument::new_from_data(bytes, "").expect("Poppler failed");
-    let page = document.get_page(0).expect("Getting page failed");
+    let document_result = poppler::PopplerDocument::new_from_data(bytes, "");
+    if let Err(error) = document_result {
+        error!(
+            "Something went wrong with poppler reading the bytes:\n{}",
+            error
+        );
+        return None;
+    }
+    let document = document_result.unwrap();
+    let page_result = document.get_page(0);
+    if page_result.is_none() {
+        error!("Something went wrong with reading the first page of the pdf");
+        return None;
+    }
+    let page = page_result.unwrap();
     let (width, height) = page.get_size();
     let width = (scale * width) as i32;
     let height = (scale * height) as i32;
-    debug!("Width: {}, Height: {}", width, height);
+    // debug!("Width: {}, Height: {}", width, height);
 
     /* Create Cairo Context for rendering and Render PDF */
-    let mut surface = ImageSurface::create(cairo::Format::ARgb32, width, height)
-        .expect("Couldn't create cairo surface");
+    let surface_result = ImageSurface::create(cairo::Format::ARgb32, width, height);
+    if let Err(error) = surface_result {
+        error!(
+            "Something went wrong with creating a cairo surface\n{}",
+            error
+        );
+        return None;
+    }
+    let mut surface = surface_result.unwrap();
     /* Block needed to destroy context before collection data from surface */
     {
-        let cr = cairo::Context::new(&surface).expect("Couldn't create cairo context");
+        let cr_result = cairo::Context::new(&surface);
+        if let Err(error) = cr_result {
+            error!(
+                "Something went wrong with creating a cairo context\n{}",
+                error
+            );
+            return None;
+        }
+        let cr = cr_result.unwrap();
         cr.set_source_rgb(1.0, 1.0, 1.0);
-        cr.paint().expect("Failed to paint");
+        if let Err(error) = cr.paint() {
+            error!("Failed to paint on the surface:\n{}", error);
+        }
         cr.scale(scale, scale);
         page.render(&cr);
     }
@@ -60,8 +94,13 @@ fn tectonic_rendering(latex_code: &str) -> (Vec<u8>, i32, i32) {
 
     debug!("Copying to clipboard");
 
-    let byte_image = surface.data().expect("Failed to get surface data").to_vec();
-    (byte_image, width, height)
+    let byte_image_result = surface.data();
+    if let Err(error) = byte_image_result {
+        error!("Failed to access surface data:\n{}", error);
+        return None;
+    }
+    let byte_image = byte_image_result.unwrap().to_vec();
+    Some((byte_image, width, height))
 }
 
 fn on_clipboard_change(cb: &mut Clipboard) {
@@ -79,23 +118,31 @@ fn on_clipboard_change(cb: &mut Clipboard) {
 
     let snippet = snip_latex(&text);
     let latex_code = string_in_template(&snippet);
-    let (raw_bytes, width, height) = tectonic_rendering(&latex_code);
+    match tectonic_rendering(&latex_code) {
+        Some((raw_bytes, width, height)) => {
+            let test_image = ImageData {
+                width: width as usize,
+                height: height as usize,
+                bytes: Cow::from(raw_bytes),
+            };
 
-    let test_image = ImageData {
-        width: width as usize,
-        height: height as usize,
-        bytes: Cow::from(raw_bytes),
-    };
+            if let Err(error) = cb.set_image(test_image) {
+                debug!("Failed to push image in to clipboard {}", error);
+                return;
+            }
 
-    cb.set_image(test_image)
-        .expect("Failed to push image in to clipboard");
-
-    Notification::new()
-        .summary("Clip Tex")
-        .body("Ready to Paste")
-        .image_path("ClipTex.png")
-        .show()
-        .expect("No ring ring, have you blocked me? :(");
+            if let Err(error) = Notification::new()
+                .summary("Clip Tex")
+                .body("Ready to Paste")
+                .image_path("ClipTex.png")
+                .show()
+            {
+                error!("For some reason i couldn't notify you but your compilation is done and in your clipboard!:\n{}",error);
+                return;
+            }
+        }
+        _ => error!("Failed to compile the code but just continueing as nothing happend =)"),
+    }
 }
 
 #[cfg(target_os = "windows")]
